0a655348b0828441d3a999a4e38595af844c8619
DejanS <dsimic94@gmail.com>
2018-05-12 20:43:00 +0200

    Task10

--- src/pages/SinglePost.vue
+++ src/pages/SinglePost.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="container">
         <h1>{{post.title}}</h1>
-        <i>{{post.createdAt}}</i>
+        <i>{{post.createdAt | formatDate}}</i>
         <hr>
         <p>{{post.text}}</p>
         <hr>
         <add-comment @onComment="commentRefresh" />
         <h2>Comments:</h2>
         <ul class="list-group" v-for="(comment, key) in comments" :key="key">
-            <li class="list-group-item">{{comment.text}}</li>
+            <li class="list-group-item">{{comment.text}}<i class="float-right">{{comment.createdAt | diffForHumans}}</i></li>
         </ul>
     </div>
 </template>
@@ -16,7 +16,10 @@
 <script>
 import {postService} from '../service/PostService'
 import AddComment from '../components/AddComment.vue'
+import { DateMixin } from '../Mixins'
 export default {
+    mixins: [ DateMixin ],
+
     components:{
         AddComment
     },
